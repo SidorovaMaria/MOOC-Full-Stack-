47081e31bb339a8116ccc6173b9d5bee090bb113
maria sidorova <sidmashav@icloud.com>
2024-12-27 16:30:04 +0000

    Exercise 2.11. 2.11: The Phonebook Step 6

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from "react";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
-
+import axios from "axios";
+import { useEffect } from "react";
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    // { name: "Arto Hellas", number: "040-123456", id: 1 },
+    // { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    // { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    // { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      const persons = response.data;
+      setPersons(persons);
+    });
+  }, []);
+
   const [filter, setFilter] = useState("");
 
   return (
