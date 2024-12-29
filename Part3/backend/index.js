22c868e08f72eda83c8d94993fe4966e57f2d776
maria sidorova <sidmashav@icloud.com>
2024-12-29 00:07:00 +0000

    Error handling no middleware

--- Part3/backend/index.js
+++ Part3/backend/index.js
@@ -57,9 +57,19 @@ app.get("/api/persons", (request, response) => {
 });
 // Get specific person
 app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      // response.status(500).end();
+      response.status(400).send({ error: "malformatted id" });
+    });
 });
 
 // Delete entry
