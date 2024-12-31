9ba7e639e74ab03208d338c5f482e278accb2f0d
maria sidorova <sidmashav@icloud.com>
2024-12-31 11:35:00 +0000

    4.5*: Helper Functions and Unit Tests, step 3

--- Part4/Backend/tests/helper_functions.test.js
+++ Part4/Backend/tests/helper_functions.test.js
@@ -33,3 +33,35 @@ describe("total likes", () => {
     assert.strictEqual(listHelper.totalLikes(blogs), 36);
   });
 });
+
+describe("favourite blog", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  test("is itself a favourite blog if one", () => {
+    const expected = {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    };
+    assert.deepStrictEqual(listHelper.favoriteBlog(listWithOneBlog), expected);
+  });
+  test("of an empty array is zero", () => {
+    assert.strictEqual(listHelper.favoriteBlog([]), 0);
+  });
+  test("of a bigger array is found", () => {
+    const expected = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    };
+    assert.deepStrictEqual(listHelper.favoriteBlog(blogs), expected);
+  });
+});
--- Part4/Backend/utils/list_helper.js
+++ Part4/Backend/utils/list_helper.js
@@ -2,15 +2,30 @@ const dummy = (blogs) => {
   return 1;
 };
 
-const totalLikes = (blogList) => {
-  if (blogList.length === 0) {
+const totalLikes = (blogsArray) => {
+  if (blogsArray.length === 0) {
     return 0; // Return 0 for an empty array
   }
-  let totalLikes = blogList.reduce((sum, blog) => sum + blog.likes, 0);
+  let totalLikes = blogsArray.reduce((sum, blog) => sum + blog.likes, 0);
   return totalLikes;
+};
+
+const favoriteBlog = (blogsArray) => {
+  if (blogsArray.length === 0) {
+    return 0;
+  }
+  const favorite = blogsArray.reduce((fav, blog) =>
+    (fav.likes || 0) > blog.likes ? fav : blog
+  );
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
