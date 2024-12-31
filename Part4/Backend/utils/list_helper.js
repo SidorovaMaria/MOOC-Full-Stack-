bbdf60068c528186c984c414ca39fbfafffdc758
maria sidorova <sidmashav@icloud.com>
2024-12-31 11:07:15 +0000

    4.4: Helper Functions and Unit Tests, step 2

--- Part4/Backend/utils/list_helper.js
+++ Part4/Backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogList) => {
+  if (blogList.length === 0) {
+    return 0; // Return 0 for an empty array
+  }
+  let totalLikes = blogList.reduce((sum, blog) => sum + blog.likes, 0);
+  return totalLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
