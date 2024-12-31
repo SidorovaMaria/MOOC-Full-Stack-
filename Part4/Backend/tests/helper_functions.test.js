40255ae69c487a2c4148c15b6cfb669f72c68188
maria sidorova <sidmashav@icloud.com>
2024-12-31 12:17:12 +0000

    4.6*: Helper Functions and Unit Tests, step 4

--- Part4/Backend/tests/helper_functions.test.js
+++ Part4/Backend/tests/helper_functions.test.js
@@ -2,6 +2,16 @@ const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 const blogs = require("../db");
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 5,
+    __v: 0,
+  },
+];
 
 test("dummy returns one", () => {
   const blogs = [];
@@ -10,16 +20,6 @@ test("dummy returns one", () => {
 });
 
 describe("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
   test("of empty list is zero", () => {
     assert.strictEqual(listHelper.totalLikes([]), 0);
   });
@@ -35,16 +35,6 @@ describe("total likes", () => {
 });
 
 describe("favourite blog", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
   test("is itself a favourite blog if one", () => {
     const expected = {
       title: "Go To Statement Considered Harmful",
@@ -63,5 +53,40 @@ describe("favourite blog", () => {
       likes: 12,
     };
     assert.deepStrictEqual(listHelper.favoriteBlog(blogs), expected);
+  });
+});
+
+describe("mostBlogs usual", () => {
+  test("if empty array returns 0 ", () => {
+    assert.strictEqual(listHelper.mostBlogs([]), 0);
+  });
+  test("return itself is one passed", () => {
+    assert.deepStrictEqual(listHelper.mostBlogs(listWithOneBlog), {
+      author: "Edsger W. Dijkstra",
+      blogs: 1,
+    });
+  });
+  test("if many array are passed the most blogs is found", () => {
+    assert.deepStrictEqual(listHelper.mostBlogs(blogs), {
+      author: "Robert C. Martin",
+      blogs: 3,
+    });
+  });
+});
+describe("mostBlogs Lodash", () => {
+  test("if empty array returns 0 ", () => {
+    assert.strictEqual(listHelper.mostBlogsLodash([]), 0);
+  });
+  test("return itself is one passed", () => {
+    assert.deepStrictEqual(listHelper.mostBlogsLodash(listWithOneBlog), {
+      author: "Edsger W. Dijkstra",
+      blogs: 1,
+    });
+  });
+  test("if many array are passed the most blogs is found", () => {
+    assert.deepStrictEqual(listHelper.mostBlogsLodash(blogs), {
+      author: "Robert C. Martin",
+      blogs: 3,
+    });
   });
 });
--- Part4/Backend/utils/list_helper.js
+++ Part4/Backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -23,9 +25,47 @@ const favoriteBlog = (blogsArray) => {
     likes: favorite.likes,
   };
 };
+// Without LODASH
+const mostBlogs = (blogsArray) => {
+  if (blogsArray.length === 0) {
+    return 0;
+  }
+  const authorBlogCounts = {};
+  //count the number of blogs for each author
+  blogsArray.forEach((blog) => {
+    authorBlogCounts[blog.author] = (authorBlogCounts[blog.author] || 0) + 1;
+  });
+  const topAuthor = Object.entries(authorBlogCounts).reduce(
+    (top, [author, count]) =>
+      count > top.blogs ? { author, blogs: count } : top,
+    { author: "", blogs: 0 }
+  );
+  return topAuthor;
+};
+const mostBlogsLodash = (blogsArray) => {
+  if (blogsArray.length === 0) {
+    return 0;
+  }
+
+  // Group blogs by author
+  const groupedBlogs = _.groupBy(blogsArray, "author");
+
+  // Map the grouped result to get each author's blog count
+  const authorBlogCounts = _.map(groupedBlogs, (blogs, author) => ({
+    author,
+    blogs: blogs.length,
+  }));
+
+  // Find the author with the most blogs
+  const topAuthor = _.maxBy(authorBlogCounts, "blogs");
+
+  return topAuthor;
+};
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostBlogsLodash,
 };
